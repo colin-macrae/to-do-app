d8a159343d06c5e90239d9f45fc59e4f57948941
Colin Macrae <csmacrae@gmail.com>
2023-10-19 16:25:43 -0700

    input form added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,32 @@
-import "./Home.css"
+import "./Home.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [toDoItems, setToDoItems] = useState([
+    { text: "", completed: false, selected: false },
+  ]);
+
+  function handleSubmit() {
+    const inputItem = document.querySelector('input[name="new-item"]').value;
+    const newItemObject = [{ text: null, completed: false, selected: false }];
+    newItemObject.text = inputItem;
+    const newAllItems = [...toDoItems, newItemObject];
+    setToDoItems(newAllItems);
+  }
+
+  console.log(toDoItems);
+
   return (
-  <section> 
-    <h1>To Dos</h1>
-  </section>
-  )
+    <section>
+      <h1>To Dos</h1>
+      <form>
+        <label htmlFor="new-item">New Item</label>
+        <input type="text" name="new-item" />
+        <button type="button" onClick={handleSubmit}>
+          Save
+        </button>
+      </form>
+      <p>{toDoItems[0].completed ? "completed" : "not completed"}</p>
+    </section>
+  );
 }
